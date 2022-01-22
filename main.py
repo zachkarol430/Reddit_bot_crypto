cd66d103205218f32becc759f0685caea914145e
zachkarol430 <zachkarol430@gmail.com>
2022-01-22 13:42:41 -0500

    gitignore test

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 import praw
-# See
 reddit = praw.Reddit(
     client_id="RGk2fXW6iRH_BRmjrtfk5w",
     client_secret="Sk39BuXFpcetaIOwltN4MMgIwN4jSw",
@@ -13,11 +10,23 @@ reddit = praw.Reddit(
 
 subreddit= "CryptoMoonshots"
 post_number= 10
+#i will make dataframe later
+titles= []
+comments=[]
 for submission in reddit.subreddit(subreddit).hot(limit=post_number):
-    print("title: " + submission.title.lower())
+    posts_title= ("title: " + submission.title.lower())
     try:
-        print(submission.comments[1].body) #ignore automod, just first comment for now. Most comments are troll
+        comment= (submission.comments[1].body) #ignore automod, just first comment for now. Most comments are troll
     except:
-        print("no comments")
+        comment= ("no comments")
+    titles.append(posts_title)
+    comments.append(comment)
+
+print(titles)
+print(comments)
+
+
+
+
 
 
